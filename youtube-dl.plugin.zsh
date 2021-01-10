7a651f385d6c27968a2df4e704df5905e4959638
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2021-01-10 13:01:59 +0100

    Improve format overview before selection prompt

--- youtube-dl.plugin.zsh
+++ youtube-dl.plugin.zsh
@@ -14,10 +14,16 @@ alias yts="ytnpl --format worstaudio --extract-audio"
 # Music
 alias ytm="ytnpl --format bestaudio --extract-audio"
 
-# List formats & prompt for one
-ytf() {
+ytf() { # List formats & prompt for which one(s) to download
 	FORMATS=$(ytnpl --list-formats "$1")
-	echo "== Please copy-paste below 👇 one 'format code' listed above ☝️"
-	read -r FORMAT
-	ytnpl --format "$FORMAT" "$1"
+	echo "$FORMATS" | grep --extended-regexp --invert-match " only"
+	echo ""
+	echo "$FORMATS" | grep --extended-regexp "video only"
+	echo ""
+	echo "$FORMATS" | grep --extended-regexp "audio only"
+	echo "" && echo "== Preparing to save download as:"
+	echo "  $(pwd)/$(youtube-dl --restrict-filenames --get-filename $1)"
+	echo "== Please copy-paste 👇 a 'format code' (or vid+aud) ☝️"
+	FORMAT=$(read -r)
+	ytnpl --restrict-filenames --format "$FORMAT" "$1"
 }
