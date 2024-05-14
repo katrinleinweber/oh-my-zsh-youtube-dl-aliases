3d6a7ff05331fafc9b24c0ebd6950c212be4a42c
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2024-05-14 21:08:46 +0200

    Update youtube-dl.plugin.zsh

--- youtube-dl.plugin.zsh
+++ youtube-dl.plugin.zsh
@@ -1,29 +1,56 @@
-alias ytnpl="youtube-dl --no-playlist --restrict-filenames"
+alias ytnpl="yt-dlp --no-playlist --restrict-filenames"
 
 # Prefer SD on YouTube
 alias yt="ytnpl  --format 18"
 alias yth="ytnpl --format 22"
 
-# For presentations
-alias ytp="ytnpl  --format 244+worstaudio"
-alias ytpp="ytnpl --format 247+worstaudio"
+# Presentations
+alias ytp="ytnpl --write-subs --write-auto-subs --format 244+249"
+
+# Programming presentations
+alias ytpp="ytnpl --write-subs --write-auto-subs --format 247+249"
 
 # Speech
-alias yts="ytnpl --format worstaudio --extract-audio"
+alias yts="ytnpl --write-subs --write-auto-subs --format worstaudio --extract-audio"
 
 # Music
 alias ytm="ytnpl --format bestaudio --extract-audio"
 
+play_latest() {
+	latest_file=$(find . -type f -ctime -1s | head -1)
+	vlc --rate 1.33 "$latest_file"
+}
+
 ytf() { # List formats & prompt for which one(s) to download
+	set -eux -pipefail
+	which ffmpeg
+
 	FORMATS=$(ytnpl --list-formats "$1")
-	echo "$FORMATS" | grep --extended-regexp --invert-match " only"
+
+	echo "$FORMATS" | grep --invert-match "(audio|video) only"
 	echo ""
-	echo "$FORMATS" | grep --extended-regexp "video only"
-	echo ""
-	echo "$FORMATS" | grep --extended-regexp "audio only"
-	echo "" && echo "== Preparing to save download as:"
-	echo "  $(pwd)/$(youtube-dl --restrict-filenames --get-filename $1)"
+	
+	for type in audio video
+	do
+		echo "$FORMATS" | grep "$type only"
+		echo ""
+	done
+
 	echo "== Please copy-paste 👇 a 'format code' (or vid+aud) ☝️"
 	read -r FORMAT
 	ytnpl --format "$FORMAT" "$1"
+	
+	play_latest
+}
+
+yw() {
+	cd /tmp
+	ytp "$1"
+	play_latest
+}
+
+yww() {
+	cd /tmp
+	yth "$1"
+	play_latest
 }
